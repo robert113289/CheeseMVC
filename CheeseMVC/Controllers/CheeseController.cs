e6dbb443d3a6276f79d57035341a8f538b5c71c2
Robert Kirkum <robert113289@gmail.com>
2017-11-13 22:17:17 -0600

    finished studio 4

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private Dictionary<string,string> Cheeses = new Dictionary<string,string>();
+
+        public IActionResult Index()
+        {
+    
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+        
+        public IActionResult Add()
+        {
+
+            return View();
+
+        }
+        [HttpPost()]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            Cheeses.Add(name,description);
+            return Redirect("/cheese");
+        }
+        
+        public IActionResult Remove()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Remove(string[] cheese)
+        {
+            foreach (string item in cheese)
+            {
+                Console.WriteLine(item);
+                if(Cheeses.ContainsKey(item))
+                {
+                    Cheeses.Remove(item);
+                }
+            }
+  
+            return Redirect("/Cheese");
+        }
+    }
+}
